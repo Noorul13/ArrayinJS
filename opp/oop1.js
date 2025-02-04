34ac9f18a931fdae0f53aa57d8d224c6cd2a5b0a
Noorul Khan <noorul.khan@mobulous.com>
2025-02-04 13:28:13 +0530

    changes

--- opp/oop1.js
+++ opp/oop1.js
@@ -5,15 +5,13 @@ class Doctor {
         this.name = name;
         this.specialty = specialty;
     }
-
     // Method to display doctor's information
     getDoctorInfo() {
         return `Dr. ${this.name}, Specialist in ${this.specialty}`;
     }
 }
-
 // Create an instance (object) of Doctor
 const doctor1 = new Doctor("Rajeev Kumar", "Cardiology");
 
 // Call the method
-//console.log(doctor1.getDoctorInfo());  // Output: Dr. Rajeev Kumar, Specialist in Cardiology
+console.log(doctor1.getDoctorInfo());  // Output: Dr. Rajeev Kumar, Specialist in Cardiology
